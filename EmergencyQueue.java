4a868b374f3226570e4655308c5fd69321b48bcf
Sahee <saheeahamed22@gmail.com>
2026-09-09 00:27:00 +0530

    Implement emergency patient queue

---
+++ EmergencyQueue.java
@@ -0,0 +1,58 @@
+public class EmergencyQueue {
+    private EmergencyNode front;
+    private EmergencyNode rear;
+
+    public EmergencyQueue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(Patient patient) {
+        if (patient == null) {
+            System.out.println("Patient details are missing.");
+            return;
+        }
+
+        EmergencyNode newNode = new EmergencyNode(patient);
+
+        if (front == null) {
+            front = newNode;
+            rear = newNode;
+            return;
+        }
+
+        rear.setNext(newNode);
+        rear = newNode;
+    }
+
+    public Patient dequeue() {
+        if (front == null) {
+            return null;
+        }
+
+        Patient removedPatient = front.getPatient();
+
+        if (front == rear) {
+            front = null;
+            rear = null;
+        } else {
+            front = front.getNext();
+        }
+
+        return removedPatient;
+    }
+
+    public void displayQueue() {
+        if (front == null) {
+            System.out.println("Emergency queue is empty.");
+            return;
+        }
+
+        EmergencyNode current = front;
+
+        while (current != null) {
+            System.out.println(current.getPatient().toString());
+            current = current.getNext();
+        }
+    }
+}
