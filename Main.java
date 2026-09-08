2584235a2875ce14fb5e82e88c7de3beef4d5643
Sahee <saheeahamed22@gmail.com>
2026-09-09 00:09:53 +0530

    Initialize hospital emergency management project

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mini Hospital Emergency Management System");
+        System.out.println("Project initialized successfully.");
+    }
+}
